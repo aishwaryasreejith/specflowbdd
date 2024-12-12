d70e3eba87cafe4a4b7a194ae362c79b2ecad9c5
aishwaryasreejith <aishwarya.sreejith@maersk.com>
2024-12-12 16:01:10 +0530

    add

--- obj/Debug/net8.0/SpecFlowNUnitProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/SpecFlowNUnitProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpecFlowNUnitProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b15b40e018e3703d68b880f472e01fd1591d76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ca171f9901baa957c283868673a5f4b12c9e45")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpecFlowNUnitProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpecFlowNUnitProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
